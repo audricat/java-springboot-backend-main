5aefcb0e099ff7c6c9e0b4528c1c80bf0cdc37b8
‎ <shanenprz@gmail.com>
2023-06-30 12:53:01 +0800

    Initial commit

---
+++ src/main/java/net/mdjava/springbootbackend/SpringbootBackendApplication.java
@@ -0,0 +1,32 @@
+package net.mdjava.springbootbackend;
+
+import net.mdjava.springbootbackend.model.Category;
+import net.mdjava.springbootbackend.repository.CategoryRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootBackendApplication implements CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootBackendApplication.class, args);
+    }
+    
+    @Autowired
+     private CategoryRepository categoryRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        // Category category = new Category();
+        // category.setName("Fragile");
+        // category.setDescription("Delivered");
+        // categoryRepository.save(category);
+        
+        // Category category1 = new Category();
+        // category1.setName("Fragile");
+        // category1.setDescription("In Transit");
+        // categoryRepository.save(category1);
+    }
+}
